8c191251aec3527123c2ea38aae955fb384bf4b4
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-11-01 11:48:25 +0600

    Update file structure

---
+++ deck.py
@@ -0,0 +1,27 @@
+import random
+from data import suits, ranks
+from card import Card
+
+
+class Deck:
+
+    def __init__(self):
+        self.card_list = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.card_list.append(Card(suit, rank))
+
+    def shuffle_deck(self):
+        random.shuffle(self.card_list)
+
+    def deal_one(self):
+        return self.card_list.pop()
+
+    def __str__(self):
+        deck_comp = ''
+
+        for card in self.card_list:
+            deck_comp += card.__str__() + '\n'
+
+        return deck_comp
